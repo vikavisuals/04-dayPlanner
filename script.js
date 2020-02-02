a6da8c8d08be56d7cec3aa607b1d7d2197074061
vkaszuba <codeword.vika@gmail.com>
2020-02-01 19:29:28 -0800

    Established a way to loop through hours and current time

--- script.js
+++ script.js
@@ -2,13 +2,51 @@
 
 
 // Shows the day of the week
-console.log(moment().format('dddd, MMMM Do YYYY'));
+console.log(moment().format('dddd, MMMM Do'));
 
 // Shows hour and minute in American formatting
-console.log(moment().format('h A'));
+console.log(moment().format('HH'));
 
 // Displays the day of the week in header
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
 $("#presentBar").text(moment().format('h A'));
+
+
+
+const hoursInDay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+console.log(hoursInDay.length);
+
+// Turns 24 Hour current time into a number
+let currentTime = parseInt(moment().format('HH'));
+
+
+function checkHour() {
+
+  for (let i = 0; i < hoursInDay.length; i++) {
+    if (hoursInDay[i] > currentTime) {
+      console.log("Green");
+    } else if (hoursInDay[i] === currentTime) {
+      console.log("Current");
+    } else {
+      console.log("Grey");
+    }
+  }
+return;
+};
+
+checkHour();
+
+
+
+console.log(hoursInDay);
+console.log(currentTime);
+
+
+
+
+// Turns 24 time into 12 hour
+currentTime = moment().format('h A');
+console.log(currentTime);
+
 
